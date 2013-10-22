3fc120a98c0528bb26192ff2b7d7fcea9e5c1bad
wilcrofter <william@william-XS36.(none)>
2013-10-22 08:08:22 -0400

    A little polish, and added openintro's cars dataset as csv to avoid confusion with standard dataset of same name. Easy stuff is done. On to handling command Output.Type via callback.

--- R/demo.R
+++ R/demo.R
@@ -1,41 +1,46 @@
 library(stringr)
-library(openintro)
 
-mod2.env <- new.env(parent = emptyenv())
+# Implement the module content and state as an environment,
+# following Hadley. 
+module <- new.env(parent = emptyenv())
 
-# Read swirl 1.0 course content into the mod2.env environment
-assign("mod2",
-       read.csv("data/mod2.csv", as.is=TRUE),
-       env=mod2.env)
+# Read swirl 1.0 course content into the module environment
+module$mod2 <- read.csv("data/mod2.csv", as.is=TRUE)
+# As a convenience, store mod2's number of rows there too.
+module$rows <- nrow(module$mod2) 
 
+# Read the cars dataset from the openintro package into the global env.
+# (It's stored as a csv in case openintro is not installed.)
+cars <- read.csv("data/cars.csv", as.is=TRUE, comment.char="#")
 
 hi <- function(){
-  # Initialize a "state", consisting of a row number, in mod2.env
-  assign("row.nr", 1, env=mod2.env)
-  # We'll need the cars dataset. For now, just hard code it.
-  data(cars)
+  # Initialize a "state", consisting of a row number and a hint flag,
+  # stored in module.
+  module$row.nr <- 1
+  module$hint <- FALSE
+  # Register function checkCmd() as that to be called
+  # upon completion of any "top level" task, i.e., a command the
+  # user enters from the R prompt.
+  addTaskCallback(checkCmd, name = "mod2")
   runState()
 }
 
-runState <- function(){
-  row.nr <- get("row.nr", mod2.env)
-  n.rows <- nrow(get("mod2", mod2.env))
-  if(row.nr <= n.rows){
-  content <- get("mod2", mod2.env)[row.nr,]
-  if(content[,1]=="text"){
-    runTextState(content)
-  } else if(content[,1]=="figure"){
-    runFigureState(content)
-  } else if(content[,1]=="video"){
-    runVideoState(content)
-  } else {
-    message("Can't do this one; run nxt()")
+nxt <- function(override=FALSE){
+  # Increment the state unless the hint flag is TRUE
+  # or the override parameter is TRUE.
+  if(!module$hint | override){
+    module$row.nr <- 1 + module$row.nr
+    module$hint <- FALSE
   }
-  } else {
-    message("That's all, folks!")
-  }
+  runState()
 }
 
+bye <- function(){
+  # Unregister callback
+  removeTaskCallback(which(getTaskCallbackNames() == "mod2"))
+}
+
+# Hadley's display function
 frndlyOut <- function(...) {
   # Format the argument for pretty display.
   # getOption("width") gives screen width in characters.
@@ -44,6 +49,24 @@ frndlyOut <- function(...) {
   # Start each line with "| " (str_c is in package stringr)
   # and display the result.
   message(str_c("| ", wrapped, collapse = "\n"))
+}
+
+runState <- function(){
+  if(module$row.nr <= module$rows){
+    content <- module$mod2[module$row.nr,]
+    if(content[,1]=="text"){
+      runTextState(content)
+    } else if(content[,1]=="figure"){
+      runFigureState(content)
+    } else if(content[,1]=="video"){
+      runVideoState(content)
+    } else {
+      message("Can't do this one; run nxt()")
+    }
+  } else {
+    bye()
+    message("That's all, folks!")
+  }
 }
 
 runTextState <- function(content){
@@ -66,8 +89,6 @@ runVideoState <- function(content){
   }
 }
 
-nxt <- function(){
-  row.nr <- get("row.nr", mod2.env)
-  assign("row.nr", row.nr+1, envir=mod2.env)
-  runState()
+checkCmd <- function(...){
+  # The callback
 }
