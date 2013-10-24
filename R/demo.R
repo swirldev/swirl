afb040f71b12d2d6f8804ca4b2f11ab453635473
wilcrofter <william@william-XS36.(none)>
2013-10-24 16:15:02 -0400

    A more or less working version.

--- R/demo.R
+++ R/demo.R
@@ -1,8 +1,11 @@
 library(stringr)
 
 # Implement the module content and state as an environment,
-# following Hadley. 
-module <- new.env(parent = emptyenv())
+# following Hadley. However, let the global environment be
+# the parent, since we will want to evaluate expressions 
+# within the new environment as a check on the user's computation.
+# (See the callback function, below.)
+module <- new.env()
 
 # Read swirl 1.0 course content into the module environment
 module$mod2 <- read.csv("data/mod2.csv", as.is=TRUE)
@@ -110,9 +113,8 @@ runQuestionState <- function(content){
     answerExact(content)
   } else if (content[,"Answer.Type"] == "range"){
     answerRange(content)
-  } else {
-    message("Can't check this one; run nxt()")
   }
+  # A command answer type will be handled by the callback
 }
 
 answerText <-function(content){
@@ -152,6 +154,32 @@ respond <- function(correct){
 }
 
 answerCmd <- function(...){
-  # The callback
+  if(!..3){
+    # Whatever the user entered cause an error
+    frndlyOut(paste("Oops! Type nxt() for a hint and try again."))
+    module$hint <- TRUE
+    return(TRUE)
+  }
+  if(identical(..1, parse(text="nxt()")[[1]]))return(TRUE)
+  if(identical(..1, parse(text="nxt(TRUE)")[[1]]))return(TRUE)
+  if(identical(..1, parse(text="hi()")[[1]]))return(TRUE)
+  content <- module$mod2[module$row.nr,]
+  # Evaluate the correct answer within environment "module"
+  # so as not to interfere with what the user has done in
+  # the global environment. Assign the result to "ans" within
+  # "module" in order to make it accessible.
+  temp <- paste("ans <-", content[,"Correct.Answer"])
+  eval(parse(text=temp), envir=module)
+  ans.is.correct <- identical(..2, module$ans)
+  correct.expr <- parse(text=content[,"Correct.Answer"])
+  call.is.correct <- identical(..1, correct.expr[[1]])
+  if(!call.is.correct && !ans.is.correct){
+    respond(FALSE)
+  } else if(!call.is.correct && ans.is.correct){
+    frndlyOut(paste("You got the right value but used a different expression for the purpose. You entered ", as.character(as.expression(..1)),", which was not what I expected. Type nxt() for a hint"))
+    module$hint <- TRUE
+  } else {
+    respond(TRUE)
+  }
   return(TRUE)
 }
