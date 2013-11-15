6dcb7ef2c0e4668c3de31b9fe7bc578e85d64e3d
Nick Carchedi <nick.carchedi@gmail.com>
2013-11-15 07:34:40 -0500

    updated saveSession to fix some errors and include a test

--- R/saveSession.R
+++ R/saveSession.R
@@ -3,18 +3,27 @@
 #' then containing any other information we wish to save (e.g. user defined variables, user's
 #' current progress, etc.)
 
-saveSession <- function(..., data) {
+saveSession <- function(expr, val, ok, vis, data) {
   # Set up directory in a native platform-compliant manner
-  dir <- file.path(path.package("swirl"), "userData")
+  fileDir <- file.path(path.package("swirl"), "userData")
   # Assuming first three elements of data are username, course, and module...
-  fileName <- paste(data[[1]], data[[2]], data[[3]], sep="_")
+  fileName <- paste0(paste(data[[1]], data[[2]], data[[3]], sep="_"), ".Rdata")
   # Specify final file path
-  filePath <- file.path(dir, fileName)
+  filePath <- file.path(fileDir, fileName)
   
   # Save user data as .Rdata file
   save(data, file=filePath)
 }
 
+######## TEST ########
+
+# Load swirl library so it can find path.package("swirl")
+library(swirl)
+
+# Define data variable, which will contain all important session data
+data <- list("ncarchedi", "data_analysis", "module2", 
+             list(a="current state", b="user-defined variables", c="other useful info"))
+
 #' Initiate callback that will cause session data (contained entirely in data 
 #' argument) to be saved automatically following every top-level task.
-addTaskCallBack(saveSession, data=data, name="autoSave")
+addTaskCallback(saveSession, data=data, name="autoSave")
