1eabb98fcf35f73e58c46ae3a86c3d198d6960fd
wilcrofter <bill@crofter.net>
2013-11-28 12:35:12 -0500

    progress on "instruction set"

--- R/testModInstr.R
+++ R/testModInstr.R
@@ -1,3 +1,5 @@
+library(stringr)
+
 #' Instruction set and utilities for testMod.R's "virtual machine".
 #' 
 #' In testMod4Daphne there are 3 Output Types, 'text', 'video', and 
@@ -27,11 +29,58 @@
 #' what the user has entered. If correct it exits the loop and indicates
 #' it is finished. If incorrect it prints the Hint and remains
 #' in the loop.
+
+#' All classes first Output, all in the same way, hence one method
+#' suffices.
 #' 
-#' All classes first Output (all in the same way,) then wait 
-#' for user response (in 4 different ways.) Text and video are
-#' both finished at this point. Call it point 2.
-#' 
+present <- function(current.state, e)UseMethod("present")
+
+present.default <- function(current.row, e){
+  swirl_out(current.row[,"Output"])
+  e$iptr <- 1 + e$iptr
+}
+
+#' All classes then wait for user response, in 4 different ways, hence
+#' 4 different methods are required. Text and video are both finished
+#' at this point.
+
+waitUser <- function(current.row, e)UseMethod("waitUser")
+
+waitUser.text <- function(current.row, e){
+  readline("...")
+  e$row <- 1 + e$row
+  e$iptr <- 1
+}
+
+waitUser.video <- function(current.row, e){
+  response <- readline("y or n? ")
+  if(response %in% c("y", "Y", "yes", "Yes")){
+    swirl_out("Type nxt() to continue")
+    e$prompt <- TRUE
+    browseURL(current.row[,"VideoLink"])
+  }
+  e$row <- 1 + e$row
+  e$iptr <- 1
+}
+
+waitUser.mult_question <- function(current.row, e){
+  # Use strsplit with split=";" to separate the choices
+  # Strsplit returns a list but we want only its first element,
+  # a vector of choices.
+  # Use str_trim (pkg stringr) to remove white space from the choices.
+  # Use select.list to get the user's choice.
+  # Return the user's choice.
+  choices <- strsplit(current.row[,"AnswerChoices"],";")
+  choices <- str_trim(choices[[1]])
+  e$val <- select.list(choices)
+  e$iptr <- 1 + e$iptr
+}
+
+waitUser.cmd_question <- function(current.row, e){
+  e$prompt <- TRUE
+  e$iptr <- 1 + e$iptr
+}
+
 #' Questions of both types enter a loop at point 2. Point 3 is testing. 
 #' Point 4 is conditional on testing. Failure prints the Hint (point 5)
 #' and returns to point 2. Success exits.
