687c760ecd330a2a6ee2a95551a812d839a91520
Gina <gina@crofter.org>
2013-12-03 20:28:54 -0500

    wrote script to add class column to csv data files

---
+++ R/fixmodule.R
@@ -0,0 +1,16 @@
+mod <- read.csv("data/Courses/Open_Intro/module1/mod1.csv",as.is=TRUE)
+nr <- nrow(mod)
+mod[,"Class"] <- NA
+cmds <- list(text = "text_question", 
+             multiple = "mult_question",
+             exact="exact_question", 
+             range="range_question",
+             command="cmd_question")
+for (n in 1:nr)
+{
+ if (mod[n,"Output.Type"]=="question")
+   class_type <- cmds[[ mod[n,"Answer.Type"] ]]
+ else 
+    class_type <- mod[n,"Output.Type"]
+ mod[n,"Class"] <- class_type
+}
