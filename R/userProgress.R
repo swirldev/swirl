89c8e1d7812888d7b4ae7356dd644aea14bf8cbc
wilcrofter <bill@crofter.net>
2013-12-05 19:15:54 -0500

    userProgress.R in progress

--- R/userProgress.R
+++ R/userProgress.R
@@ -1,19 +1,23 @@
 source("R/testMod.R")
 
 initSwirl.userProgress <- function(e){
-  # Sign up?
-  #   => capture username
-  #   create directory
-  # Else sign in
-  # e$usr <- username
-  # find stored progress
-  # if previous, continue previous?
-  #   => restore progress including relevant parts of e
-  # else choose new module
-  #   => partially populate e
-  initSwirl.default(e) # temporary
-  #      Create a list for expressions entered by user
-  e$usrexpr <- list()
+  e$usr <- "swirladmin"   # stub for sign in or sign up
+  # Find or create progress storage location
+  udat <- file.path(find.package("swirl"), "user_data", e$usr)
+  if(!file.exists(udat))dir.create(udat)
+  # Check for the existence of progress files
+  pfiles <- dir(udat)[grep("[.]rda", dir(udat))]
+  # Would the user care to continue with any of these?
+  selection <- "No thanks"
+  if(length(pfiles) > 0){
+    selection <- select.list("Would you like to continue with one of these modules?", c(pfiles, selection))
+  }
+  if(selection == "No thanks"){
+    initSwirl.default(e) 
+    e$usrexpr <- list()
+  } else {
+    # restore selection
+  }
 }
 
 #' This function is invoked prior to processing
@@ -22,7 +26,7 @@ initSwirl.userProgress <- function(e){
 #' value of e$expr should be saved unless seen
 #' the last time.
 saveProgress.userProgress <-  function(e){
-  if(!identical(e$usrexpr[[1]], e$expr)){
+  if(length(e$usrexp)==0 || !identical(e$usrexpr[[1]], e$expr)){
     e$usrexpr <- c(e$expr, e$usrexpr)
   }
   # save e to disk
