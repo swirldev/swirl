d07da8f5db1b6544360f57bec78e905a459f806e
Nick Carchedi <nick.carchedi@gmail.com>
2014-01-08 12:16:33 -0500

    significantly improved and comprehensive rmd to csv converter for swirl modules

--- R/rmd2csv.R
+++ R/rmd2csv.R
@@ -1,107 +1,134 @@
-# This script takes content authored in an Rmd file and puts it in a csv file.
-# This is a covenience for instructors who prefer to author content in Rmd format.
-# It will be refined over time as we revisit course authoring best practices.
-# NOTE: The csv will have the same name as the Rmd file and will be placed in the
-# same directory. Must uncomment final write.csv() to use!
+library(stringr)
 
-clean_rmd <- function(rmd) {
-  
-  # Remove leading and trailing whitespace
+clean_me <- function(rmd) {
+    # Remove leading and trailing whitespace
   rmd_clean <- str_trim(rmd)
   
   # Remove empty lines
-  rmd_clean[which(rmd_clean != "")]
+  rmd_clean <- rmd_clean[which(rmd_clean != "")]
+  
+  # Remove title
+  rmd_clean[-c(1, 2)]
 }
 
 into_units <- function(rmd) {
-  
   # Separate rmd into groups based on units of instruction
-  unit_num <- cumsum(rmd == "---")
+  unit_num <- cumsum(str_detect(rmd, fixed("---")))
   
   # Return list of units
   split(rmd, unit_num)
 }
 
-collapse_choices <- function(choices) {
-  no_num <- sub("^[1-9][.]\\s", "", choices)
-  paste(no_num, collapse = "; ")
+get_unit_class <- function(unit) {
+  str_split_fixed(unit[1], "&", 2)[2]
 }
 
-parse_unit <- function(unit) {
- 
-  # Remove dashes if they are there
-  dash_ind <- grep("^---$", unit)
-  if(length(dash_ind) > 0) unit <- unit[-dash_ind]
-  
+get_corr_ans <- function(unit) UseMethod("get_corr_ans")
+
+get_corr_ans.default <- function() {
+  message("Unit class not specified!")
+}
+
+get_corr_ans.cmd_question <- function(unit) {
   # Find code chunk delimeters
   beg_chunk <- grep("```{r", unit, fixed=TRUE)
   end_chunk <- grep("^```$", unit)
   
-  if(length(beg_chunk) > 0 && length(end_chunk) > 0) {
-    # Run this block only if code chunk exists
-    # Define output to be everything before code chunk begins
-    cl <- "cmd_question"
-    output <- unit[seq(1, beg_chunk - 1)]
-    
-    # Take correct answer to be everything in between (exclusive)
-    correct_ans <- unit[seq(beg_chunk + 1, end_chunk - 1)]
-    
-  } else {
-    # Run this if no code chunk exists
-    cl <- "text"
-    output <- unit
-    correct_ans <- NA
+  if(length(beg_chunk) == 0 | length(end_chunk) == 0) {
+    error("You forgot to specify the correct answer on a command question!")
   }
   
+  # Return everything in between (exclusive)
+  unit[seq(beg_chunk + 1, end_chunk - 1)]
+}
+
+get_corr_ans.mult_question <- function(unit) {
+  corr_ans_ind <- grep("^_[1-9][.].+_$", unit)
+  if(length(corr_ans_ind) == 0) {
+    error("You forgot to specify the correct answer on a multiple choice question!")
+  }
+  gsub("^_[1-9][.]\\s|_$", "", unit[corr_ans_ind])
+}
+
+collapse_choices <- function(choices) {
+  no_num <- gsub("^_?[1-9][.]\\s|_?$", "", choices)
+  paste(no_num, collapse = "; ")
+}
+
+get_ans_choices <- function(unit) {
   # Find answer choices
-  choice_ind <- grep("^[1-9][.]", unit)
-  # Set choices to NA
-  choices <- NA
-  
-  # If answer choices exist
-  if(length(choice_ind) > 0) {
-    cl <- "mult_question"
-    output <- unit[-choice_ind]
-    choices <- collapse_choices(unit[choice_ind])
+  choice_ind <- grep("^_?[1-9][.]", unit)
+  if(length(choice_ind) == 0) {
+    error("You forgot to specify answer choices!")
   }
+  # Collapse answer choices
+  collapse_choices(unit[choice_ind])
+}
+
+get_ans_tests <- function(unit) {
+  ans_tests_ind <- grep("*** .ans_tests", unit, fixed = TRUE) + 1
+  if(length(ans_tests_ind) == 0) {
+    error("You forgot to specify answer tests!")
+  }
+  unit[ans_tests_ind]
+}
+
+get_hint <- function(unit) {
+  hint_ind <- grep("*** .hint", unit, fixed = TRUE) + 1
+  if(length(hint_ind) == 0) error("You forgot to specify a hint!")
+  hint <- unit[hint_ind]
+}
+
+make_row <- function(unit) UseMethod("make_row")
+
+make_row.default <- function(unit) {
+  message("You forgot to specify a content class!")
+}
+
+make_row.text <- function(unit) {
+  output <- unit[2]
   
-  # Return output and correct answer
-  list(cl = cl, output = output, correct_ans = correct_ans, choices = choices)
+  c(Class = class(unit), Output = output, CorrectAnswer = NA,
+       AnswerChoices = NA, AnswerTests = NA, Hint = NA,
+       Figure = NA, FigureType = NA, VideoLink = NA)
+}
+
+make_row.cmd_question <- function(unit) {
+  output <- unit[2]
+  corr_ans <- get_corr_ans(unit)
+  ans_tests <- get_ans_tests(unit)
+  hint <- get_hint(unit)
+  
+  c(Class = class(unit), Output = output, CorrectAnswer = corr_ans,
+       AnswerChoices = NA, AnswerTests = ans_tests, Hint = hint,
+       Figure = NA, FigureType = NA, VideoLink = NA)
+}
+
+make_row.mult_question <- function(unit) {
+  output <- unit[2]
+  corr_ans <- get_corr_ans(unit)
+  ans_choices <- get_ans_choices(unit)
+  ans_tests <- paste0("word=", corr_ans)
+  hint <- get_hint(unit)
+  
+  c(Class = class(unit), Output = output, CorrectAnswer = corr_ans,
+       AnswerChoices = ans_choices, AnswerTests = ans_tests, Hint = hint,
+       Figure = NA, FigureType = NA, VideoLink = NA)
 }
 
 rmd2csv <- function(rmd_path) {
-  # Read Rmd file with output and correct answers only
-  my_rmd <- readLines(rmd_path, n = 500)
+  my_rmd <- readLines(rmd_path)
+  cleaned <- clean_me(my_rmd)
+  units <- into_units(cleaned)
+  classes <- lapply(units, get_unit_class)
+  units_with_class <- mapply(`class<-`, units, classes)
+  rows <- sapply(units_with_class, make_row)
+  df <- as.data.frame(t(rows))
   
-  # Remove title
-  my_rmd <- my_rmd[-c(1, 2)]
-  
-  # Remove whitespace
-  my_rmd_clean <- clean_rmd(my_rmd)
-  
-  # Break rmd into units
-  units <- into_units(my_rmd_clean)
-  
-  # Vectorize parsing over units
-  content <- lapply(units, parse_unit)
-  
-  # Create df columns
-  cl <- sapply(content, `[[`, 1)
-  output <- sapply(content, `[[`, 2)
-  correct_ans <- sapply(content, `[[`, 3)
-  choices <- sapply(content, `[[`, 4)
-  
-  # Organize in data frame
-  df <- data.frame(Class = cl, Output = output, CorrectAnswer = correct_ans, 
-                   AnswerChoices = choices, AnswerTests = NA, Hint = NA, 
-                   Figure = NA, VideoLink = NA, Tag = NA)
-  
-  # Create path to csv
-  csv_path <- sub(".Rmd$", ".csv", rmd_path)
-
-  # Write data frame to csv
+  # Write content data frame to csv file with same name as rmd
+  csv_path <- sub("[.][R|r]md", ".csv", rmd_path)
   write.csv(df, file = csv_path, row.names = FALSE)
 }
 
-#fp <- "./inst/Courses/Intro to R/module1/mod1_new.Rmd"
-#rmd2csv(fp)
+#rmd_path <- "./inst/Courses/Intro_to_R/module1/mod1_new.Rmd"
+rmd2csv(rmd_path)
