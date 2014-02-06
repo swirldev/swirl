ca6bc3df30a7d01b832dd3049421fe512653951f
seankross <smk240@gmail.com>
2014-02-06 12:22:09 -0500

    added documentation for install_course

--- R/install_course.R
+++ R/install_course.R
@@ -1,21 +1,34 @@
-# Install a course from a zipped course folder
-
+#' Install a course from a zipped course folder
+#' 
+#' @param path The path to the zipped course.
+#' @export
+#' @examples
+#' install_course_zip("~/Desktop/my_course.zip")
 install_course_zip <- function(path){
   # Unzip file into courses
   unzip(path, exdir=file.path(path.package("swirl"), "Courses"))
 }
 
-# Install a course from a directory which contains modules
-
+#' Install a course from a course directory
+#' 
+#' @param path The path to the course directory.
+#' @export
+#' @examples
+#' install_course_directory("~/Desktop/my_course")
 install_course_directory <- function(path){
   # Copy files
   file.copy(path, file.path(path.package("swirl"), "Courses"), recursive=T)
 }
 
-# Must use the httr package
-
-# Install a course from a github repository
-
+#' Install a course from a GitHub repository
+#' 
+#' @param github.username The username that owns the course repository.
+#' @param course.name The name of the repository which should be the name of the course.
+#' @param branch The branch of the repository containing the course. The default branch is \code{"master"}.
+#' @export
+#' @examples
+#' install_course_github("bcaffo", "Linear_Regression")
+#' install_course_github("jtleek", "Twiiter_Map", "geojson")
 install_course_github <- function(github.username, course.name, branch="master"){
   
   # Construct url to the zip file
@@ -24,6 +37,12 @@ install_course_github <- function(github.username, course.name, branch="master")
   install_course_url(zip_url, type="github", course.name=course.name)
 }
 
+#' Install a course from a zipped course directory shared on Dropbox
+#' 
+#' @param url URL of the shared file
+#' @export
+#' @examples
+#' install_course_dropbox("https://www.dropbox.com/s/xttkmuvu7hh72vu/my_course.zip")
 install_course_dropbox <- function(url){
   # Construct url to the zip file
   zip_url <- paste0(sub("www.dropbox", "dl.dropboxusercontent", url), "?dl=1")
@@ -31,19 +50,26 @@ install_course_dropbox <- function(url){
   install_course_url(zip_url)
 }
 
+#' Install a course from a zipped course directory shared on Google Drive
+#' 
+#' @param url URL of the shared file
+#' @export
+#' @examples
+#' install_course_google_drive("https://drive.google.com/file/d/F3fveiu873hfjZZj/edit?usp=sharing")
 install_course_google_drive <- function(url){
   # Construct url to the zip file
   first_sub <- sub("/edit\\?usp=sharing", "", url)
-  zip_url <- sub("file/d/", "uc?export=download&id=", first_sub)
+  zip_url <- sub("file/d/", "uc?export=download&id=", sub("/edit\\?usp=sharing", "", url))
   
   install_course_url(zip_url)
 }
 
-# install_course_github("seankross", "test_course")
-
-# Install a course from a url that points to a zip file
-#This needs to be tested
-
+#' Install a course from a url that points to a zip file
+#' 
+#' @param url URL that points to a zipped course directory
+#' @export
+#' @examples
+#' install_course_url("http://www.biostat.jhsph.edu/~rpeng/File_Hash_Course.zip")
 install_course_url <- function(url, type="url", course.name=""){
   # Send GET request
   response <- GET(url)
