88cc502156f2fbbb0d560370adf24b77b246f6ed
wilcrofter <bill@crofter.net>
2014-02-11 09:58:59 -0500

    Made omnitest demo more informative and entertaining.

--- inst/Courses/Test_Modules/module3/mod3.R
+++ inst/Courses/Test_Modules/module3/mod3.R
@@ -4,11 +4,13 @@ savemod <- function(){
   write.csv(module, modpath, row.names=FALSE)
 }
 
-newrow <- function(Class=NA, Output=NA, CorrectAnswer=NA, AnswerChoices=NA, AnswerTests=NA, 
-                               Hint=NA, Figure=NA, FigureType=NA, VideoLink=NA){
+newrow <- function(Class=NA, Output=NA, CorrectAnswer=NA, 
+                   AnswerChoices=NA, AnswerTests=NA, Hint=NA, Figure=NA,
+                   FigureType=NA, VideoLink=NA){
   temp <- data.frame(Class=Class, Output=Output, CorrectAnswer=CorrectAnswer, 
-                     AnswerChoices=AnswerChoices, AnswerTests=AnswerTests, Hint=Hint,
-                     Figure=Figure, FigureType=FigureType, VideoLink=VideoLink)
+                     AnswerChoices=AnswerChoices, AnswerTests=AnswerTests, 
+                     Hint=Hint, Figure=Figure, FigureType=FigureType, 
+                     VideoLink=VideoLink)
 }
 
 module <- newrow(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
@@ -16,22 +18,22 @@ module <- newrow(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
 # just text, no question
 module <- rbind(module, newrow(
   Class="text", 
-  Output="This is test module for function omnitest. Omnitest can test for a correct expression, a correct value, or both. In the case of values it is limited to testing for character or numerical vectors of length 1."
+  Output="This is a brief demonstration of function omnitest. Omnitest can grade student answers for a correct expression, a correct value, or both. In the case of values it is limited to testing for character or numerical vectors of length 1. Omnitest's general signature is: omnitest(correctExpr=NULL, correctVal=NULL, strict=FALSE)"
   ))
-
 # multiple choice question
 module <- rbind(module, newrow(
   Class="mult_question",
-  Output="Testing for a correct e$val given a multiple choice question. Which movie involve Pierce Brosnan in a singing role?",
+  Output="First, I'll use omnitest(correctVal='Mamma Mia!') for the answer to a multiple choice question: Which movie involves Pierce Brosnan in a singing role?",
   AnswerChoices="Mamma Mia!;Guys and Dolls;Once is Not Enough",
   CorrectAnswer="Mamma Mia!",
-  AnswerTests="omnitest(correctVal='Mamma Mia!')"
+  AnswerTests="omnitest(correctVal='Mamma Mia!')",
+  Hint = "In Guys and Dolls, Marlon Brando sings. In Once is Not Enough, 007 doesn't."
   ))
 
 # User must give an exact, numerical answer
 module <- rbind(module, newrow(
   Class="exact_question",
-  Output="Testing for an exact numerical answer entered at the command line. How many Stooges are there?",
+  Output="Next, I'll use omnitest(correctVal=3) for an exact numerical answer entered at the command line: How many Stooges are there?",
   CorrectAnswer=3,
   AnswerTests="omnitest(correctVal=3)",
   Hint="This one should be obvious! Nyuk, Nyuk, Nyuk!"
@@ -40,16 +42,16 @@ module <- rbind(module, newrow(
 # requires user to enter a command
 module <- rbind(module, newrow(
   Class="cmd_question",
-  Output="Testing that a user has entered a particular command. Enter myVar <- c(3, 5, 7)",
-  CorrectAnswer="myVar <- c(3, 5, 7)",
-  AnswerTests="omnitest(correctExpr='myVar <- c(3, 5, 7)')",
-  Hint="Enter myVar <- c(3, 5, 7)"
+  Output="Now I'll use omnitest(correctExpr='scooby <- c(1969, 2013)') to test that a user has entered a particular command: Enter scooby <- c(1969, 2013)",
+  CorrectAnswer="scooby <- c(1969, 2013)",
+  AnswerTests="omnitest(correctExpr='scooby <- c(1969, 2013)')",
+  Hint="So far, Scooby has been on the air or in theaters from 1969 to 2013. Enter scooby <- c(1969, 2013)"
   ))
 
 # requires user to enter a command
 module <- rbind(module, newrow(
   Class="cmd_question",
-  Output="Testing that a user has entered a command which computes a specific value but perhaps in a different manner than anticipated. The correct expression is 3^2, but enter 3*3.",
+  Output="Omnitest has strict and casual modes. I'll demonstrate casual, the default, by using omnitest(correctExpr='3^2', correctVal=9) to test that a user has entered a command which computes a specific value but perhaps in a different manner than anticipated. The correct expression is 3^2, but enter 3*3.",
   CorrectAnswer="3^2",
   AnswerTests="omnitest(correctExpr='3^2', correctVal=9)",
   Hint="Enter 3^2"
@@ -58,7 +60,7 @@ module <- rbind(module, newrow(
 # requires user to enter a command
 module <- rbind(module, newrow(
   Class="cmd_question",
-  Output="Testing that a user has entered a command which computes a specific value in a particular way. The correct expression is 3^2. Entering 3*3 should fail.",
+  Output="Finally, I'll demonstrate strict mode for same question, using omnitest(correctExpr='3^2', correctVal=9, strict=TRUE) to test that a user has entered a command which computes a specific value in a particular way. The correct expression is 3^2. Entering 3*3 should fail.",
   CorrectAnswer="3^2",
   AnswerTests="omnitest(correctExpr='3^2', correctVal=9, strict=TRUE)",
   Hint="Enter 3^2"
@@ -67,5 +69,5 @@ module <- rbind(module, newrow(
 # just text, no question
 module <- rbind(module, newrow(
   Class="text", 
-  Output="That completes the omnitest test. Thank you."
+  Output="That completes the omnitest demo. Thank you."
   ))
