88bae637f05a864aa82e786e48dd4ee6ccb97ec5
Nick Carchedi <nick.carchedi@gmail.com>
2014-02-19 22:46:52 -0500

    fixed removal of temporary variables in e

--- R/menu.R
+++ R/menu.R
@@ -94,7 +94,7 @@ mainMenu.default <- function(e){
       e$les <- loadLesson(e, courseU, lesson)
       # Remove temp lesson name and course name vars, which were surrogates
       # for csv attributes -- they've been attached via lesson() by now
-      rm(e$temp_lesson_name, e$temp_course_name)
+      rm(temp_lesson_name, temp_course_name, envir=e)
       # expr, val, ok, and vis should have been set by the callback.
       # The lesson's current row
       e$row <- 1
