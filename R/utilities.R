5a54af23839ca78a644c0b9451f8048e1397b3d9
wilcrofter <bill@crofter.net>
2014-05-16 16:57:49 -0400

    possible fix to "play" bug.

--- R/utilities.R
+++ R/utilities.R
@@ -60,7 +60,8 @@ mergeLists <- function(sourceList, destList){
 safeEval <- function(expr, e){
   e1 <- cleanEnv(e$snapshot)
   ans <- list()
-  suppressMessages(suppressWarnings(eval(expr,e1)))
+  temp <- try(suppressMessages(suppressWarnings(eval(expr,e1))), silent=FALSE)
+  if(is(temp, "try-error"))return(ans)
   for (x in ls(e1)){
     if(exists(x,globalenv()))
       ans[[x]] <- get(x,globalenv())
@@ -103,7 +104,7 @@ cleanEnv <- function(snapshot){
   } else {
     new.env()  
   }
-  parent.env(pe) <- parent.env(globalenv())
+  parent.env(pe) <- globalenv()
   # return new environment whose parent is pe
   return(new.env(parent=pe))
 }
