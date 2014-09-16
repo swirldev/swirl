d34811ef1ba1967ff0915eae2497194c9f44e3e9
odeleongt <odeleongt@gmail.com>
2014-09-16 15:54:15 -0600

    Use names enclosed between underscores (*e.g.* _name_) as markers to replace text on lesson load.

--- R/parse_content.R
+++ R/parse_content.R
@@ -1,3 +1,4 @@
+
 find_lesson <- function(lesson_dir) {
   # Find 'lesson' file with or without extension
   grep("^lesson[.]?[A-Za-z]*$", list.files(lesson_dir), value=TRUE)
@@ -11,9 +12,26 @@ get_content_class <- function(file_name) {
 
 
 # Parse a lesson text to generate content from R expressions defined in the Parse field
-parse_text <- function(df, marker){
-	parsed <- eval(parse(text=df$Parse))
-	df <- lapply(df, function(element) gsub(marker, parsed, element, fixed=TRUE))
+parse_element <- function(df, expressions){
+	
+	# Evaluate the expressions
+	expressions <- sapply(unlist(expressions), function(x) eval(parse(text=x)))
+	
+	parse_text <- function(text){
+		if(!is.na(text)){
+			# Find the markers
+			found <- gregexpr('_([^_]*)_', text)
+			markers <- gsub('_', '', unlist(regmatches(text, found)), fixed=TRUE)
+			
+			# Fill in with the results of the expressions
+			parsed <- expressions[markers]
+			
+			regmatches(text, found) <- list(parsed)
+		}
+		text
+	}
+	
+	df <- lapply(df, parse_text)
 	df <- as.data.frame(df)
 	df	
 }
@@ -44,20 +62,22 @@ parse_content.yaml <- function(file, e){
     temp <- data.frame(Class=NA, Output=NA, CorrectAnswer=NA,
                        AnswerChoices=NA, AnswerTests=NA, 
                        Hint=NA, Figure=NA, FigureType=NA, 
-                       VideoLink=NA, Script=NA, Parse=NA)
+                       VideoLink=NA, Script=NA)
+    
     for(nm in names(element)){
       # Only replace NA with value if value is not NULL, i.e. instructor
-      # provided a nonempty value
-      if(!is.null(element[[nm]])) {
+      # provided a nonempty value. Ignore the Parse field.
+      if(!is.null(element[[nm]]) & nm!='Parse') {
         temp[,nm] <- element[[nm]]
       }
     }
-    
-    # Parse lesson defined content to prevent locale specific issues
-    if(any(!is.na(temp$Parse))) temp <- parse_text(df=temp, marker=meta$Parse)
-    
-    temp$Parse <- NULL
-    
+
+    # Parse lesson defined content to replace defined markers with the result
+    # of the named expressions in the Parsed field of the element
+    if(any(!is.null(element$Parse))){
+    	temp$Parse <- NULL
+    	temp <- parse_element(df=temp, expressions=list(element$Parse))
+    }
     temp
   }
   raw_yaml <- yaml.load_file(file)
@@ -66,10 +86,7 @@ parse_content.yaml <- function(file, e){
   
   temp <- lapply(raw_yaml[-1], newrow)
   df <- NULL
-  
-  for(row in temp){
-    df <- rbind(df, row)
-  }
+  df <- do.call(rbind.data.frame, temp)
   
   lesson(df, lesson_name=meta$Lesson, course_name=meta$Course,
          author=meta$Author, type=meta$Type, organization=meta$Organization,
