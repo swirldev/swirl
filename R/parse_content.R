59570381ff8ef1dc0bb8e28627469eb930d0981b
odeleongt <odeleongt@gmail.com>
2014-09-17 08:42:49 -0600

    Use whisker to replace text in lesson definition file prior to loading.

--- R/parse_content.R
+++ R/parse_content.R
@@ -1,4 +1,3 @@
-
 find_lesson <- function(lesson_dir) {
   # Find 'lesson' file with or without extension
   grep("^lesson[.]?[A-Za-z]*$", list.files(lesson_dir), value=TRUE)
@@ -9,33 +8,6 @@ get_content_class <- function(file_name) {
   ext <- file_ext(file_name)
   tolower(ext)
 }
-
-
-# Parse a lesson text to generate content from R expressions defined in the Parse field
-parse_element <- function(df, expressions){
-	
-	# Evaluate the expressions
-	expressions <- sapply(unlist(expressions), function(x) eval(parse(text=x)))
-	
-	parse_text <- function(text){
-		if(!is.na(text)){
-			# Find the markers
-			found <- gregexpr('_([^_]*)_', text)
-			markers <- gsub('_', '', unlist(regmatches(text, found)), fixed=TRUE)
-			
-			# Fill in with the results of the expressions
-			parsed <- expressions[markers]
-			
-			regmatches(text, found) <- list(parsed)
-		}
-		text
-	}
-	
-	df <- lapply(df, parse_text)
-	df <- as.data.frame(df)
-	df	
-}
-
 
 ### FUNCTIONS THAT RETURN LESSON OBJECT WITH ASSOCIATED ATTRIBUTES ###
 
@@ -63,32 +35,52 @@ parse_content.yaml <- function(file, e){
                        AnswerChoices=NA, AnswerTests=NA, 
                        Hint=NA, Figure=NA, FigureType=NA, 
                        VideoLink=NA, Script=NA)
-    
     for(nm in names(element)){
       # Only replace NA with value if value is not NULL, i.e. instructor
-      # provided a nonempty value. Ignore the Parse field.
-      if(!is.null(element[[nm]]) & nm!='Parse') {
+      # provided a nonempty value
+      if(!is.null(element[[nm]])) {
         temp[,nm] <- element[[nm]]
       }
-    }
-
-    # Parse lesson defined content to replace defined markers with the result
-    # of the named expressions in the Parsed field of the element
-    if(any(!is.null(element$Parse))){
-    	temp$Parse <- NULL
-    	temp <- parse_element(df=temp, expressions=list(element$Parse))
     }
     temp
   }
   raw_yaml <- yaml.load_file(file)
-  # Get metadata (including marker for text parsing)
-  meta <- raw_yaml[[1]]
-  
   temp <- lapply(raw_yaml[-1], newrow)
   df <- NULL
-  df <- do.call(rbind.data.frame, temp)
-  
+  for(row in temp){
+    df <- rbind(df, row)
+  }
+  meta <- raw_yaml[[1]]
   lesson(df, lesson_name=meta$Lesson, course_name=meta$Course,
          author=meta$Author, type=meta$Type, organization=meta$Organization,
          version=meta$Version)
+}
+
+
+#' @importFrom tools file_ext
+#' @importFrom tools file_path_sans_ext
+#' @importFrom yaml yaml.load_file
+#' @importFrom whisker whisker.render
+localize_lesson <- function(file, lesPath, shortname){
+	browser()
+	# Load localization file
+	locale_yaml <- yaml.load_file(file)
+	
+	# TO DO: Process localization. Placeholder loads first element.
+	locale_yaml <- locale_yaml[[1]]
+	
+	# Load lesson file
+	lesson <- readLines(con = file.path(lesPath, shortname))
+	
+	# Render localized lesson
+	lesson <- whisker.render(template = lesson, data = locale_yaml)
+	
+	# Save localized lesson as a temporary file
+	file <- file_path_sans_ext(shortname)
+	extension <- paste0(".", file_ext(shortname))
+	localized_lesson <- tempfile(pattern = file, fileext = extension)
+	writeLines(lesson, con = localized_lesson)
+	
+	# Return file path for localized lesson
+	return(localized_lesson)
 }
