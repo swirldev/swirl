768b5d92ce446aa2cb6779e279e702ecb9857d76
seankross <smk240@gmail.com>
2014-09-26 15:04:19 -0400

    added function to delete user progress

--- R/progress.R
+++ R/progress.R
@@ -4,3 +4,14 @@ saveProgress.default <- function(e){
   # save progress
   suppressMessages(suppressWarnings(saveRDS(e, e$progress)))
 }
+
+# Provide a user name
+deleteProgress <- function(user){
+  # Find path to user data
+  path <- system.file("user_data", user, package = "swirl")
+  
+  # Delete all files within a user folder
+  if(file.exists(path)){
+    invisible(file.remove(list.files(path, full.names = TRUE), recursive = TRUE))
+  }
+}
