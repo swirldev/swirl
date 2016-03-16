2ad13dc6dfef98ad753010705f91897b63c62809
seankross <smk240@gmail.com>
2016-03-16 10:41:44 -0400

    use stringi in encoding test

--- tests/testthat/test-encoding.R
+++ tests/testthat/test-encoding.R
@@ -1,5 +1,7 @@
 context("encoding")
 
+library(stringi)
+
 test_that("Trying to parse the test-encoding.yaml", {
   test_parse <- function(file) {
     class(file) <- get_content_class(file)
@@ -9,7 +11,5 @@ test_that("Trying to parse the test-encoding.yaml", {
   test_path <- system.file(file.path("test", "test-encoding.yaml"), package = "swirl")
   suppressWarnings(result <- test_parse(test_path))
   console <- capture.output(result)
-  wush_path <- system.file(file.path("test", "test-encoding.txt"), package = "swirl")
-  wush <- readLines(con = wush_path, warn = FALSE, encoding = "UTF-8")
-  expect_equal(strsplit(console[3], "\\s+")[[1]][3], wush)
+  expect_equal(stri_escape_unicode(strsplit(console[3], "\\s+")[[1]][3]), stri_escape_unicode("中文測試"))
 })
