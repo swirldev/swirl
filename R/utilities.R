43080db449f3972a5876d49c8141213e3aecc989
Robin <rduque1@users.noreply.github.com>
2016-10-05 11:04:05 +0200

    Extra security in complete_part()
    up_through should never be higher than the total number of exercises in a lesson.

--- R/utilities.R
+++ R/utilities.R
@@ -159,7 +159,7 @@ loadDependencies <- function(lesson_dir) {
 
 # Execute correct answers for rows 1 through 'up_through' of lesson
 complete_part <- function(e) {
-  up_through <- e$test_from - 1
+  up_through <- min(e$test_from - 1, nrow(e$les))
   # Get rows though 'up_through' argument
   les <- e$les[seq(up_through), ]
   # Execute previous correct answers in global env
